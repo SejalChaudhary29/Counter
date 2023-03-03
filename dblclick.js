b61a26e0c1710ee0671890b9c97dc8b2c4624860
Sejal Chaudhary <123448383+SejalChaudhary29@users.noreply.github.com>
2023-03-03 16:25:53 +0530

    Add files via upload

---
+++ dblclick.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let count = 0;   
+    const value = document.querySelector("#value");
+    const btns = document.querySelectorAll(".btnop");
+   
+    btns.forEach(function (btnop) {
+      btnop.addEventListener("dblclick", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("btn1")) {
+          count--;
+        } else if (styles.contains("btn3")){
+          count++;
+        } else {
+          count = 0 ;
+        } 
+        if (count > 0 ) {
+          value.style.color ="#b5e7f4";
+        }
+        if (count < 0 ) {
+          value.style.color ="#977C79";
+        }
+        if (count === 0 ) {
+          value.style.color ="#fff"; 
+        }
+        value.textContent = count;
+      });
+    });
+  });
